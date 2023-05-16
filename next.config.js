b696416b5e9487653f042cc55e97c41e5ad9e649
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2023-05-16 17:26:44 +0900

    [fix] Netlify側からbasePathを正しく認識できるようにする

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  basePath: process.env.NODE_ENV === "production" ? "/" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/" : "",
 }
 
 module.exports = nextConfig
