d4ae685474cb24810384ddcf6db181daa67c07f1
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2023-05-16 19:30:27 +0900

    [update] scrollオプションの指定

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,9 @@ export default function Header() {
   return (
     <header className={styles.wrapper}>
       <div className={styles.title}><a href="/">ohyama4z{"'"}s portfolio</a></div>
-      <div className={styles.navigation}><Link href="#about">About</Link></div>
-      <div className={styles.navigation}><Link href="#works">Works</Link></div>
-      <div className={styles.navigation}><Link href="#skills">Skills</Link></div>
+      <div className={styles.navigation}><Link href="#about" scroll={false}>About</Link></div>
+      <div className={styles.navigation}><Link href="#works" scroll={false}>Works</Link></div>
+      <div className={styles.navigation}><Link href="#skills" scroll={false}>Skills</Link></div>
     </header>
   )
 }
